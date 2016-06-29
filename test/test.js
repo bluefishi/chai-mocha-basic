d61a7de6558767016cd79cf44c27fec76ba2d713
Apple Wen <wenjianj@cn.ibm.com>
2016-06-29 08:08:30 +0800

    My first chai mocha test.

---
+++ test/test.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect
+
+describe('A basic chai mocha testing', function(){
+    it('True is always equals to true', function(){
+        expect(true).to.be.true;
+    });
+});
